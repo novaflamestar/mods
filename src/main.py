9670197d5baf05f6c94f266f1549c9d4cac00970
novaflamestar <novaflamestar@gmail.com>
2025-11-12 21:13:01 -0800

    swap to use discord bot

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+import discord
+from discord.ext import commands
+import os
 
-app = FastAPI()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
-@app.get("/")
-def main():
-    return {"message": "pong"}
+
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="-mods ", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("online")
+
+
+@bot.command(name="ban")
+async def ping(ctx, member: discord.Member):
+    await ctx.send(f"banning {member.mention}...")
+
+bot.run(TOKEN)
