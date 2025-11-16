5d836a04590b1c8c3d557fd7aa3cd157402cda7a
novaflamestar <novaflamestar@gmail.com>
2025-11-15 17:44:35 -0800

    adds status

--- src/main.py
+++ src/main.py
@@ -13,8 +13,8 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 guild_id = discord.Object(id=GUILD_ID)
 
 class AppClient(Client):
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
+    def __init__(self, *, intents: discord.Intents, activity: discord.Activity = None):
+        super().__init__(intents=intents, activity=activity)
         self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
@@ -22,7 +22,10 @@ class AppClient(Client):
         await self.tree.sync(guild=guild_id)
 
 intents = discord.Intents.all()
-client = AppClient(intents=intents)
+client = AppClient(
+    intents=intents,
+    activity=discord.Activity(type=discord.ActivityType.watching, name="ur mum kek"),
+)
 
 @client.tree.command(description="Bans a member from the server. (not really but you know it feels good)")
 async def mods_ban(interaction: discord.Interaction, member: discord.Member):
@@ -32,4 +35,6 @@ async def mods_ban(interaction: discord.Interaction, member: discord.Member):
 async def ping_error(interaction: discord.Interaction, error):
     await interaction.response.send_message("An error occurred, please annoy david to fix it.")
 
-client.run(TOKEN)
+client.run(
+    TOKEN, 
+)
